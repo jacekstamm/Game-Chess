fdadfc3167d0bc471caf755c3d353ce543185b1d
jacekstamm <jacekstamm@gmail.com>
2018-11-24 11:39:41 +0100

    Cała plansa pojawia się na tle

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,31 +1,51 @@
 package chess;
 
-import javax.swing.*;
-import java.awt.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
 
 
 class Board {
-    private JPanel square[][] = new JPanel[8][8];
 
-    public void display() {
-        JFrame frame = new JFrame("Chess");
-        frame.setSize(600, 600);
-        frame.setResizable(false);
-        frame.setLayout(new GridLayout(8, 8));
+    Image brownLight = new Image("file/BrownLight.png");
+    Image brownDark = new Image("file/BrownDark.png");
 
-        for(int i = 0; i < 8; i++) {
-            for(int j = 0; j < 8; j++) {
-                square[i][j] = new JPanel();
-                if((i + j) % 2 == 0) {
-                    square[i][j].setBackground(Color.LIGHT_GRAY);
-                } else {
-                    square[i][j].setBackground(Color.DARK_GRAY);
-                }
-                frame.add(square[i][j]);
-            }
-        }
+    public GridPane chessBoardDisplay() {
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        ImageView a1 = new ImageView();
+        a1.setImage(brownDark);
+
+        ImageView a2 = new ImageView();
+        a2.setImage(brownLight);
+
+        ImageView a3 = new ImageView();
+        a3.setImage(brownDark);
+
+        ImageView a4 = new ImageView();
+        a4.setImage(brownLight);
+
+        ImageView a5 = new ImageView();
+        a5.setImage(brownDark);
+
+        ImageView a6 = new ImageView();
+        a6.setImage(brownLight);
+
+        ImageView a7 = new ImageView();
+        a7.setImage(brownDark);
+
+        GridPane grid = new GridPane();
+
+        GridPane.setConstraints(a1, 0, 0);
+        GridPane.setConstraints(a2, 1, 0);
+        GridPane.setConstraints(a3, 2, 0);
+        GridPane.setConstraints(a4, 0, 1);
+        GridPane.setConstraints(a5, 1, 1);
+        GridPane.setConstraints(a6, 2, 1);
+
+        grid.getChildren().setAll(a1, a2, a3, a4, a5, a6);
+
+        return grid;
     }
 }
+
--- src/main/java/chess/TryRunner.java
+++ src/main/java/chess/TryRunner.java
@@ -6,7 +6,6 @@ package chess;
 public class TryRunner {
 
     public static void main(String[] args) {
-        Board board = new Board();
-        board.display();
+
     }
 }
