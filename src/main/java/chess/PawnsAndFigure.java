a14a6e59398f96c4296f6107ca709af6f5907a17
jacekstamm <jacekstamm@gmail.com>
2018-11-29 21:53:11 +0100

    Plansza poprawiona

--- src/main/java/chess/PawnsAndFigure.java
+++ src/main/java/chess/PawnsAndFigure.java
@@ -1,15 +1,10 @@
 package chess;
 
 import javafx.geometry.Insets;
-import javafx.scene.Cursor;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-
-import java.awt.*;
 
 public class PawnsAndFigure {
 
@@ -23,10 +18,10 @@ public class PawnsAndFigure {
             //Pawns
             ImageView blackPawn = new ImageView("file/Pawn Black.png");
             ImageView whitePawn = new ImageView("file/Pawn White.png");
-            GridPane.setConstraints(blackPawn, x, 0);
+            GridPane.setConstraints(blackPawn, x, 1);
             GridPane.setConstraints(whitePawn, x, 6);
             pawnsAndFigurePane.getChildren().addAll(blackPawn, whitePawn);
-
+            //Empty Spots
             for (int y = 2; y < 7; y++) {
                 Text text = new Text();
                 text.setText(" ");
@@ -38,7 +33,7 @@ public class PawnsAndFigure {
             if (x == 0 || x == 7) {
                 ImageView blackRook = new ImageView("file/Rook Black.png");
                 ImageView whiteRook = new ImageView("file/Rook White.png");
-                GridPane.setConstraints(blackRook, x, 1);
+                GridPane.setConstraints(blackRook, x, 0);
                 GridPane.setConstraints(whiteRook, x, 7);
                 pawnsAndFigurePane.getChildren().addAll(blackRook, whiteRook);
             }
@@ -46,7 +41,7 @@ public class PawnsAndFigure {
             if (x == 1 || x == 6) {
                 ImageView blackKnight = new ImageView("file/Knight Black.png");
                 ImageView whiteKnight = new ImageView("file/Knight White.png");
-                GridPane.setConstraints(blackKnight, x, 1);
+                GridPane.setConstraints(blackKnight, x, 0);
                 GridPane.setConstraints(whiteKnight, x, 7);
                 pawnsAndFigurePane.getChildren().addAll(blackKnight, whiteKnight);
             }
@@ -54,7 +49,7 @@ public class PawnsAndFigure {
             if (x == 2 || x == 5) {
                 ImageView blackBishop = new ImageView("file/Bishop Black.png");
                 ImageView whiteBishop = new ImageView("file/Bishop White.png");
-                GridPane.setConstraints(blackBishop, x, 1);
+                GridPane.setConstraints(blackBishop, x, 0);
                 GridPane.setConstraints(whiteBishop, x, 7);
                 pawnsAndFigurePane.getChildren().addAll(blackBishop, whiteBishop);
             }
@@ -62,7 +57,7 @@ public class PawnsAndFigure {
             if (x == 3) {
                 ImageView blackQueen = new ImageView("file/Queen Black.png");
                 ImageView whiteQueen = new ImageView("file/Queen White.png");
-                GridPane.setConstraints(blackQueen, x, 1);
+                GridPane.setConstraints(blackQueen, x, 0);
                 GridPane.setConstraints(whiteQueen, x, 7);
                 pawnsAndFigurePane.getChildren().addAll(blackQueen, whiteQueen);
             }
@@ -70,7 +65,7 @@ public class PawnsAndFigure {
             if (x == 4) {
                 ImageView blackKing = new ImageView("file/King Black.png");
                 ImageView whiteKing = new ImageView("file/King White.png");
-                GridPane.setConstraints(blackKing, x, 1);
+                GridPane.setConstraints(blackKing, x, 0);
                 GridPane.setConstraints(whiteKing, x, 7);
                 pawnsAndFigurePane.getChildren().addAll(blackKing, whiteKing);
             }
