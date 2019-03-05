aed0fa21fc3f2e6e2c0d12e4fa1e0f134cba7b4b
jacekstamm <jacekstamm@gmail.com>
2019-03-05 17:53:52 +0100

    validate rook

--- src/main/java/chess/Rook.java
+++ src/main/java/chess/Rook.java
@@ -15,21 +15,47 @@ public class Rook extends Figure {
         return move && collision;
     }
 
-    private boolean validateCollision(Integer fromX, Integer fromY, Integer toX, Integer toY, Figure[][] currentBoardStatus) {
-        boolean valid = false;
-        int differenceX = Math.abs(fromX - toX);
-        int differenceY = Math.abs(fromY - toY);
-
-        for (int x = 0; x < differenceX; x++) {
-            for (int y = 0; y < differenceY; y++) {
-
-            }
-        }
-        return valid;
-    }
-
     private boolean validateMove(Integer fromX, Integer fromY, Integer toX, Integer toY) {
 
         return (fromX.equals(toX) && !fromY.equals(toY)) || (!fromX.equals(toX) && fromY.equals(toY));
+    }
+
+    private boolean validateCollision(Integer fromX, Integer fromY, Integer toX, Integer toY, Figure[][] currentBoardStatus) {
+        boolean valid = false;
+        int different;
+
+        if (fromX.equals(toX)) {
+            different = Math.abs(fromY - toY);
+        } else {
+            different = Math.abs(fromX - toX);
+        }
+
+        for (int x = 0; x < different; x++) {
+            if (fromX.equals(toX) && fromY < toY) {
+                if (!currentBoardStatus[toX][fromY + x].getName().equals("Null")) {
+                    valid = true;
+                    break;
+                }
+            }
+            if (fromX.equals(toX) && fromY > toY) {
+                if (!currentBoardStatus[toX][fromY - x].getName().equals("Null")) {
+                    valid = true;
+                    break;
+                }
+            }
+            if (fromX > toX && fromY.equals(toY)) {
+                if (!currentBoardStatus[fromX - x][toY].getName().equals("Null")) {
+                    valid = true;
+                    break;
+                }
+            }
+            if (fromX < toX && fromY.equals(toY)) {
+                if (!currentBoardStatus[fromX + x][toY].getName().equals("Null")) {
+                    valid = true;
+                    break;
+                }
+            }
+        }
+        return valid;
     }
 }
